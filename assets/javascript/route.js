3ddc4cd2c22fe35941287f39da83ad9edb489d92
Dil2000 <dnks2000@gmail.com>
2017-06-22 20:22:10 -0400

    places update

--- assets/javascript/route.js
+++ assets/javascript/route.js
@@ -56,6 +56,62 @@
 }
 //findLocations();
 
+// Travis
+
+var markerLocations = [
+    ["Capitol building", 33.749,-84.388111, "Atlanta", 0],
+    ["Georiga Dome", 33.757707,-84.400835, "Atlanta", 1],
+    ["Spaghetti Junction", 33.891792,-84.25908, "Atlanta", 2],
+    ["Test", 32.657876,-96.817802, "Dallas", 3],
+    ["Test 2", 40.701464,-74.010184, "New York City", 4],
+    ["Alcatraz Island", 37.826733, -122.42284, "San Francisco", 5],
+    ["Statue of Liberty", 40.689247, -74.0445, "New York", 6],
+    ["Brooklyn Bridge", 40.706234, -73.99699, "New York", 7],
+    ["The Alamo", 29.425772, -98.48581, "San Antonio", 8],
+    ["Glacier National Park", 48.759613, -113.787022, "West Glacier", 9],
+    ["Rocky Mountain National Park", 40.366342, -105.561041, "Dennver", 10],
+    ["Port of Miami", 25.778518, -80.178033, "Miami", 11],
+    ["Disney's Magic Kingdom", 28.418742, -81.581175, "Orlando", 12] ,
+    ["Golden Gate Bridge", 37.819528, -122.478484, "San Francisco", 13],
+    ["Lake Tahoe", 39.106927, -120.041346, "San Francisco", 14],
+    ["Yellowstone National Park", 44.440724, -110.581395, "Cheyenne", 15],
+    ["Washington Monument", 38.889430, -77.035264, "Washington D.C.",16],
+    ["Niagra Falls", 43.089561, -79.069241, "Buffalo",17],
+    ["Cloud Gate", 41.882683, -87.623316, "Chicago",18],
+    ["Milwaukee Art Museum", 43.039298, -87.897010, "Milwaukee",19],
+    ["Gateway Arch", 38.624565, -90.184891, "St. Louis",20],
+    ["Monument Valley", 36.986878, -110.080999, "St. Phoenix",21],
+    ["Great Salt Lake Desert", 40.611077, -113.452747, "Salt Lake City",22],
+    ["Mount Rushmore National Memorial", 43.878864, -103.459075, "Rapid City",23],
+    ["San Andreas Fault", 35.133485, -119.6714535, "Rapid City",24],
+    ["Las Vegas Strip", 36.109643, -115.172615, "Las Vegas",25],
+    ["Cedar Point", 41.486756, -82.686739, "Cleveland",26],
+    ["Space Needle", 47.620482, -122.349128, "Seattle",27],
+    ["Liberty Memorial", 39.081011, -94.586024, "Kansas City",28],
+    ["Liberty Memorial", 39.081011, -94.586024, "Kansas City",29],
+    ["Stone Mountain", 33.806051, -84.146783, "Atlanta",30],
+    ["Andrew Jackson's Hermitage", 36.215011, -86.612985, "Nashville",31],
+    ["Pilgrim Monument", 42.052254, -70.188645, "Provincetown",32],
+    ["Hollywood Sign", 34.134062, -118.321634, "Los Angeles",33],
+    ["Four Corners Monument", 36.998996, -109.045209, "Albuquerque",34],
+    ["Southernmost Point", 24.546509, -81.797495, "Key West",35],
+    ["Monument Rocks", 38.792915, -100.762839, "Wichita",36],
+    ["Mt Elbert", 39.117811, -106.445275, "Denver",37],
+    ["Hoover Dam", 36.016226, -114.737241, "Las Vegas",38]
+   ];
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /******* Find the flight route locations ****************************/  
@@ -156,7 +212,6 @@
           }              
         }
       } 
-      PointersOntheMap();
       return;
       },
       error: function(data, text) { alert('Failed to fetch flight: ' + data); },
@@ -192,34 +247,52 @@
 
     flightPath.setMap(map);
 
+/****************************** Travis ******************************************/
+/*
+   for( i = 0; i < markers.length; i++ ) {
+      var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+      bounds.extend(position);
+      marker = new google.maps.Marker({
+          position: position,
+          map: map,
+          title: markers[i][0],
+          icon: {
+            url: "assets/images/test.png",
+            scaledSize: new google.maps.Size(64, 64)
+          },
+          visible: false
+      });
+
+*/
+
 /******* Find cities close to the the route ****************************/  
 
-    setTimeout(
+   /* setTimeout(
       function() 
         { // Just to keep it wait till the route is done
-        }, 5000);
+        }, 5000); */
 /*
     var lastPoint = "{lat:" + lastLati + " , lng: " + lastLong + "}";
     var firstPoint = "{lat:" + firstLati + " , lng: " + firstLong + "}";
-    console.log("last point : " + lastPoint + " firstPoint : " + firstPoint);
+    console.log("last point : " + lastPoint + " firstPoint : " + firstPoint); */
 
     infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(map);
     // Location 1 to search
     //console.log('>>>>>>>>> ' + lastLati);
-   /* service.nearbySearch({
-    location: lastPoint, // replace with firstPoint
-    radius: 500,
-    type: ['(landmarks)'],
-    animation: google.maps.Animation.DROP
-    }, callback); */
-    // Location 2 to search
-   /* service.nearbySearch({
-      location: {lat:26.150 , lng: -80.153}, // replace with lastPoint
-      radius: 500,
-      type: ['(landmarks)'],
-      animation: google.maps.Animation.DROP
-    }, callback);
+
+
+     for( i = 0; i < markerLocations.length; i++ ) {
+        var latitu = {lat:  markerLocations[i][1] , lng: markerLocations[i][2] };
+
+        service.nearbySearch({
+        location: latitu, //{lat:26.150 , lng: -80.153}, // replace with firstPoint
+        //location: {lat:26.150 , lng: -80.153},
+        radius: 500,
+        type: ['stadium','landmarks'],
+        animation: google.maps.Animation.DROP
+        }, callback); 
+      }
 
     // Create pointers in the map
     function callback(results, status) {
@@ -234,59 +307,20 @@
       var placeLoc = place.geometry.location;
       var marker = new google.maps.Marker({
         map: map,
-        position: place.geometry.location
+        position: place.geometry.location,
+        icon: {
+          url: "assets/image/test.png",
+          scaledSize : new google.maps.Size(64,64)
+        }
       });
       google.maps.event.addListener(marker, 'click', function() {
         infowindow.setContent(place.name);
         infowindow.open(map, this);
       });
-    }; */
+    }; 
+
+
   };
-
-  function PointersOntheMap(){
-        // basic map settings
-        console.log("inside");
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 5,
-      center: {lat: 37.6334167, lng: -90.9594444},
-      mapTypeId: 'terrain'
-    });
-
-    var lastPoint = "{lat:" + lastLati + " , lng: " + lastLong + "}";
-    var firstPoint = "{lat:" + firstLati + " , lng: " + firstLong + "}";
-    console.log("last point : " + lastPoint + " firstPoint : " + firstPoint);
-
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-
-    service.nearbySearch({
-      location: {lat:26.150 , lng: -80.153}, // replace with lastPoint
-      radius: 500,
-      type: ['(landmarks)'],
-      animation: google.maps.Animation.DROP
-    }, callback);
-
-    // Create pointers in the map
-    function callback(results, status) {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-          createMarker(results[i]);
-        }
-      }
-    }
-
-    function createMarker(place) {
-      var placeLoc = place.geometry.location;
-      var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-      });
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-      });
-    };
-  }
 
 
   $('#go_button').on ("click", function() {
